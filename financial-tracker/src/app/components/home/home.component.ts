910e3ebb96a591a929cf36d08ecee0fbc01a6b73
carlhiramyaro <carlhiramyaro@outlook.com>
2024-12-11 23:19:54 -0500

    home page looks good

--- financial-tracker/src/app/components/home/home.component.ts
+++ financial-tracker/src/app/components/home/home.component.ts
@@ -11,10 +11,23 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  amount: number = 0;
+  description: string = '';
+  date: string = '';
+
   constructor(private router: Router) {}
 
   navigateTo(route: string) {
     // This will be implemented when you add these routes
     this.router.navigate([`/${route}`]);
+  }
+
+  proceedToCategory() {
+    // Implementation will go here
+    console.log('Proceeding with:', {
+      amount: this.amount,
+      description: this.description,
+      date: this.date,
+    });
   }
 }
